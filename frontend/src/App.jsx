eb7860edbe91a7a65aac56f3d0e2e5b3bfa72d61
Sazeebul Bashar <sazeebul@gmail.com>
2024-06-18 17:43:50 +0600

    json model done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from 'axios';
+import Spinners from './components/Spinners';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData]= useState([]);
+  const [loading, setLoading]= useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    const response = await axios.get("http://127.0.0.1:8000/api/get-data");
+    // const json = await response.json();
+    const resData = await response.data.data;
+    setLoading(false);
+    setData(resData);
+  };
+
+  useEffect(() => {
+
+    fetchData();
+    console.log(data);
+
+    
+  }, []);
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container my-5">
+      <h1 className="text-center my-5">Table Data Visualization</h1>
+      {loading && <Spinners/>}
+        {!loading && (
+          <table className="table  table-success table-striped-columns" border={3}>
+          <caption className="text-center">Table Visualization</caption>
+
+            <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Trade Code</th>
+              <th scope="col">High</th>
+              <th scope="col">Low</th>
+              <th scope="col">Open</th>
+              <th scope="col">Close</th>
+              <th scope="col">Volume</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+
+              {
+                data.map((item) => {
+                    return(
+                      <tr>
+                      <th scope="row">{item.date}</th>
+                      <td>{item.trade_code}</td>
+                      <td>{item.high}</td>
+                      <td>{item.low}</td>
+                      <td>{item.open}</td>
+                      <td>{item.close}</td>
+                      <td>{item.volume}</td>
+                    </tr>
+                    );
+                  
+                })
+              }
+
+            
+
+          </tbody>
+        </table>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
